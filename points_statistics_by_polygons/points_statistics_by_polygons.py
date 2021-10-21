7c15297d2295848263c178edf87ba4e4c8cf86ee
Abdelghani Tassi <tassiabdelghani@gmail.com>
2021-10-21 23:56:32 +0200

    point statistics by polygons script args parser

--- points_statistics_by_polygons/points_statistics_by_polygons.py
+++ points_statistics_by_polygons/points_statistics_by_polygons.py
@@ -1,2 +1,13 @@
+import argparse
+
 if __name__ == '__main__':
-    print ("hello world !")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output', required=True)
+    parser.add_argument('-i', '--pointsLayer', required=True)
+    parser.add_argument('-r', '--polygonsLayer', required=True)
+    parser.add_argument('-a', '--attributeStats', required=True)
+    args = parser.parse_args()
+    
+    print (args)
+    
