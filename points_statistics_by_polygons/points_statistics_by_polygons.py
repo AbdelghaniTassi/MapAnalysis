69eac2f18322294709ee2b5c1f60e4a04a7ad0b6
Abdelghani Tassi <tassiabdelghani@gmail.com>
2021-10-25 23:07:56 +0200

    add points ststs by polygons algorithm

--- points_statistics_by_polygons/points_statistics_by_polygons.py
+++ points_statistics_by_polygons/points_statistics_by_polygons.py
@@ -1,8 +1,20 @@
 import argparse
-
+from geopandas.tools import sjoin
+import geopandas as gpd
 
 def execute(pointsLayer, polygonsLayer, attributeStats, output):
-    return 0
+    # read points file
+    points = gpd.read_file(pointsLayer)
+    # read polygons file
+    polygons - gpd.read_file(polygonsLayer)
+    # join points with polygons
+    points_polys = gpd.sjoin(points, polys, how="left")
+    # compute statistics
+    stats_pt = points_polys.groupby('id_right')[attributeStats].agg(['mean','std','max','min'])
+    # merge stats with polygons layer
+    result = pd.merge(polys, stats_pt , left_on='id',right_index=True,how='outer')
+    # write result to output file
+    result.to_file(output)
 
 if __name__ == '__main__':
 
@@ -12,6 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--polygonsLayer', required=True)
     parser.add_argument('-a', '--attributeStats', required=True)
     args = parser.parse_args()
-    
-    print (args)
+
+    # launch algorithm
+    execute(args.pointsLayer, args.polygonsLayer, args.attributeStats, args.output)
     
