f136b762ab263009f9e05189b5ad40f14f642c44
Abdelghani Tassi <abdelghani0013@gmail.com>
2021-10-27 21:17:01 +0200

    fix var name

--- points_statistics_by_polygons/points_statistics_by_polygons.py
+++ points_statistics_by_polygons/points_statistics_by_polygons.py
@@ -6,13 +6,15 @@ def execute(pointsLayer, polygonsLayer, attributeStats, output):
     # read points file
     points = gpd.read_file(pointsLayer)
     # read polygons file
-    polygons - gpd.read_file(polygonsLayer)
+    polygons = gpd.read_file(polygonsLayer)
     # join points with polygons
-    points_polys = gpd.sjoin(points, polys, how="left")
+    points_polys = gpd.sjoin(points, polygons, how="left")
     # compute statistics
     stats_pt = points_polys.groupby('id_right')[attributeStats].agg(['mean','std','max','min'])
     # merge stats with polygons layer
-    result = pd.merge(polys, stats_pt , left_on='id',right_index=True,how='outer')
+    result = pd.merge(polygons, stats_pt , left_on='id',right_index=True,how='outer')
+
+    print (output)
     # write result to output file
     result.to_file(output)
 
@@ -24,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--polygonsLayer', required=True)
     parser.add_argument('-a', '--attributeStats', required=True)
     args = parser.parse_args()
-
+    print (args)
     # launch algorithm
     execute(args.pointsLayer, args.polygonsLayer, args.attributeStats, args.output)
     
