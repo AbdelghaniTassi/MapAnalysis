6d970bdc63bdeb2d4cf0fb1cd778bea8e0785649
Abdelghani Tassi <abdelghani0013@gmail.com>
2022-02-13 19:13:10 +0100

    refacto code

--- points_statistics_by_polygons/points_statistics_by_polygons.py
+++ points_statistics_by_polygons/points_statistics_by_polygons.py
@@ -11,21 +11,19 @@ def execute(pointsLayer, polygonsLayer, attributeStats, output):
     # read polygons file
     polygons = gpd.read_file(polygonsLayer)
     print(polygons)
+    
     # join points with polygons
     points_polys = gpd.sjoin(points, polygons, how="left")
     print (points_polys)
-    # compute statistics
 
-    
+    # compute statistics
     stats_pt = points_polys.groupby('index_right')[attributeStats].agg(['mean','count','max','min'])
-    
     print (stats_pt)
-    print (len(stats_pt))
 
     # merge stats with polygons layer
     result = pd.concat([polygons, stats_pt], axis=1)
-
     print (result)
+
     # write result to output file
     result.to_file(output)
 
