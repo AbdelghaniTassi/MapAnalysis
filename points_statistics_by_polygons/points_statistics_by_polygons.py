a687132bdb1fb10c7982b1a339d82040f470a9ae
Abdelghani Tassi <abdelghani0013@gmail.com>
2022-02-13 17:35:42 +0100

    fix merge in points groupby polygon

--- points_statistics_by_polygons/points_statistics_by_polygons.py
+++ points_statistics_by_polygons/points_statistics_by_polygons.py
@@ -1,20 +1,31 @@
 import argparse
 from geopandas.tools import sjoin
 import geopandas as gpd
+import pandas as pd
+
 
 def execute(pointsLayer, polygonsLayer, attributeStats, output):
     # read points file
     points = gpd.read_file(pointsLayer)
+
     # read polygons file
     polygons = gpd.read_file(polygonsLayer)
+    print(polygons)
     # join points with polygons
     points_polys = gpd.sjoin(points, polygons, how="left")
+    print (points_polys)
     # compute statistics
-    stats_pt = points_polys.groupby('id_right')[attributeStats].agg(['mean','std','max','min'])
-    # merge stats with polygons layer
-    result = pd.merge(polygons, stats_pt , left_on='id',right_index=True,how='outer')
 
-    print (output)
+    
+    stats_pt = points_polys.groupby('index_right')[attributeStats].agg(['mean','count','max','min'])
+    
+    print (stats_pt)
+    print (len(stats_pt))
+
+    # merge stats with polygons layer
+    result = pd.concat([polygons, stats_pt], axis=1)
+
+    print (result)
     # write result to output file
     result.to_file(output)
 
